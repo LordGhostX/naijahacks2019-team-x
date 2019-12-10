ef4b7b06d63a6c3deb51009dbbfaa74bcf019f17
LordGhostX <solomonghost9@gmail.com>
2019-12-10 03:43:27 +0000

    implemented subscribe form

--- API/app.py
+++ API/app.py
@@ -1,12 +1,20 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, request, render_template, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route("/")
-@app.route("/index.html")
+@app.route("/", methods=["POST", "GET"])
+@app.route("/index.html", methods=["POST", "GET"])
 def index():
+    form_type = request.form.get("formType", None)
+    if form_type == "subscribe":
+        email = request.form.get("email")
+        add_subscriber(email)
+        return render_template("index.html", subscribed=True)
+
     return render_template("index.html")
+
+def add_subscriber(subscriber):
+    open("lists/subscribers.txt", "a+").write(subscriber + "\n")
 
 if __name__ == "__main__":
     app.run(debug=True)
